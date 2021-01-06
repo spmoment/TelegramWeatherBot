7384fdb8af88602a9701168d98b83cd821dd9bf6
Stepan Rodionov <stwork93@mail.ru>
2021-01-06 12:59:54 +0300

    first

---
+++ src/main/java/Weather.java
@@ -0,0 +1,42 @@
+import jdk.nashorn.api.scripting.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+
+    public static String getWeather(String message, Model model) throws IOException {
+//        c334fce2c9ea3faa68718ef52cac3933
+//        http://api.openweathermap.org/data/2.5/weather?q=Penza&units=metric&appid=c334fce2c9ea3faa68718ef52cac3933
+        URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=c334fce2c9ea3faa68718ef52cac3933");
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+        model.setHumidity(main.getDouble("humidity"));
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String) obj.get("icon"));
+            model.setMain((String) obj.get("main"));
+
+        }
+        return "City: " + model.getName() + "\n" +
+                "Temperature: " + model.getTemp() + "C" + "\n" +
+                "Humidity:" + model.getHumidity() + "%" + "\n" +
+                "Main: " + model.getMain() + "\n" +
+                "http://openweathermap.org/img/w/" + model.getIcon() + ".png";
+    }
+
+}
